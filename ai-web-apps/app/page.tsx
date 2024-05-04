2f2f4d8a49319d188ac3070eb8b164f3f829357d
GhosteeGaming2103 <pedroza2015.dp@gmail.com>
2024-05-04 00:10:54 -0500

    Created the general outline for the main website, created simple node js api for the chatbot and a simple chatbox ui for the interaction.

---
+++ ai-web-apps/app/page.tsx
@@ -0,0 +1,10 @@
+import Chatbox from "@/components/ui/chatbox";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <Chatbox />
+    </main>
+  );
+}
