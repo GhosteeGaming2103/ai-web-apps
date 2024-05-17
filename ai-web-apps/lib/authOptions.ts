749d73c3995abc28a005b3eea1e8ead7173a3f5a
GhosteeGaming2103 <pedroza2015.dp@gmail.com>
2024-05-16 23:40:42 -0500

    Added check user to authOptions

--- ai-web-apps/lib/authOptions.ts
+++ ai-web-apps/lib/authOptions.ts
@@ -1,6 +1,7 @@
 
 import axios from "axios";
 import GithubProvider from "next-auth/providers/github"
+import mysql from "mysql2/promise";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -21,6 +22,30 @@ export const authOptions = {
         //   email: user.email,
         // });
         // console.log("RESPONE: ", response.data);
+        let temp = undefined;
+        const connection = await mysql.createConnection({
+          host: process.env.DATABASE_HOST,
+          user: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+        });
+        // Check if the email already exists
+        const [rows, fields] = await connection.execute(
+          "SELECT * FROM users WHERE email = ?",
+          [user.email]
+        );
+        if (Array.isArray(rows) && rows.length === 0) {
+          await connection.execute("INSERT INTO users (email) VALUES (?)", [
+            user.email,
+          ]);
+          const [rows] = await connection.execute("SELECT * FROM users WHERE email = ?", [user.email]);
+          temp = rows as any;
+          user.id = temp[0].id;
+        }
+        else {
+          temp = rows as any;
+          user.id = temp[0].id;
+        }
         const userId = 8
         user.id = userId;
         return true
